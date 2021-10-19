2697b873233d66170ea62bb8f64c3bf18af0b385
cclefjp <cclefjp@outlook.com>
2021-10-19 10:04:17 +0900

    デバッグメッセージを追加

--- cclefnotes/functions.php
+++ cclefnotes/functions.php
@@ -1,5 +1,8 @@
 <?php
 /* functions.php - cclef notes theme用の関数群 */
 
+echo 'functions.phpを読み込んでいます。';
 // tfp_include_webfonts()
 include_once('functions/cnt_include_webfonts.php');
+
+echo 'function.phpの読み込みが終了しました。';

34e7cf1aee814706770c8a4f1762b9c262be7a24
cclefjp <cclefjp@outlook.com>
2021-10-19 09:42:30 +0900

    initial commit

---
+++ cclefnotes/footer.php
@@ -0,0 +1,27 @@
+<!-- すべてのページのフッタ部分を記載 -->
+</div><!-- page-body -->
+            <footer class="footer" id="footer">
+                <div class="footerContents">
+                    <div class="footer-navi">
+                        <nav class="footer-navi">
+                        <?php
+                        wp_nav_menu(
+                            array (
+                                'theme_location' => 'footer-navi',
+                            )
+                            );
+                        ?>
+                        </nav>
+                    </div><!-- footer-navi -->
+                    <div class="sns-link">
+                        <ul class="sns-link">
+                            <?php echo tfp_get_sns_link(); ?>
+                        </ul>
+                    </div><!-- sns-link -->
+                    <div class="copyright"> &copy;<?php echo tfp_get_copyright_statement(); ?> </div>
+                </div><!-- footerContents -->
+            </footer>
+        </div> <!-- container -->
+        <?php wp_footer(); ?>
+    </body>
+</html>
---
+++ cclefnotes/functions.php
@@ -0,0 +1,5 @@
+<?php
+/* functions.php - cclef notes theme用の関数群 */
+
+// tfp_include_webfonts()
+include_once('functions/cnt_include_webfonts.php');

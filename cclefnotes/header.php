22f71dcb8654e34dd714b8db1b5df22f57d46a19
cclefjp <cclefjp@outlook.com>
2021-10-19 10:43:12 +0900

    デバッグメッセージを追加

--- cclefnotes/header.php
+++ cclefnotes/header.php
@@ -8,16 +8,19 @@
         <link rel="stylesheet" href="<?php echo get_stylesheet_uri(); ?>">
         <title><?php get_the_title(); ?></title>
         <?php wp_head(); ?>
+        <?php echo 'headタグまで終了しました'; ?>
     </head>
     <body <?php body_class(); ?>>
         <div class="container">
             <header id="header" style="background: <?php echo cnt_get_header_img_cssstyle( 'no-repeat' ); ?> background-size: cover;">
                 <div class="header-inner">
                     <?php the_widget('CNT_LogoWidget'); ?>
+                    <?php echo '1'; ?>
                     <div class="header-searchbox">
                         <?php get_search_form(true); ?>
                     </div>
                     <div class="header-navi">
+                    <?php echo '2'; ?>
                         <nav class="header-navi">
                         <?php wp_nav_menu(
                             array ( 
@@ -30,11 +33,13 @@
                     color: <?php echo cnt_get_header_font_color(); ?>;
                     font-family: <?php echo cnt_get_title_font_family(); ?>;
                     ">
+                                            <?php echo '3'; ?>
                         <?php echo cnt_get_page_title(); ?>
                     </div><!-- page-title -->
                 </div> <!-- header-inner -->
             </header>
             <div class="page-body">
+            <?php echo '4'; ?>
             	<?php if ( function_exists( 'cnt_breadcrumb' )): ?>
               	<div class="breadcrumb">
 									<?php cnt_breadcrumb(); ?>

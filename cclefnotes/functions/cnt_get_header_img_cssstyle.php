e8aa36100b6d10e9bdeda6711fa2a19eca0db833
cclefjp <cclefjp@outlook.com>
2021-10-19 10:52:10 +0900

    cnt_get_header_img.phpのインクルードを追加

--- cclefnotes/functions/cnt_get_header_img_cssstyle.php
+++ cclefnotes/functions/cnt_get_header_img_cssstyle.php
@@ -1,8 +1,10 @@
 <?php
 
+include_once('cnt_get_header_img.php');
+
 /* ヘッダー画像をCSSスタイルで返す url(...) */
 function cnt_get_header_img_cssstyle( $css_option ) {
-    $imgurl = tfp_get_header_img();
+    $imgurl = cnt_get_header_img();
     if ( $imgurl ) {
         $imgcss = 'url(' . $imgurl . ') ' . $css_option . ';';
         return $imgcss;

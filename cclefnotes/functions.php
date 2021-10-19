dd7c3fea37dbcd7f5b6f7bb4ecec0fa58d239a18
cclefjp <cclefjp@outlook.com>
2021-10-19 11:55:27 +0900

    includeを追加

--- cclefnotes/functions.php
+++ cclefnotes/functions.php
@@ -3,6 +3,18 @@
 
 echo 'functions.phpを読み込んでいます。';
 
+/* カスタムメニューを有効化する */
+register_nav_menus(
+    array(
+    'header-navi' => 'Header Menu',
+    'footer-navi' => 'Footer Menu'
+    )
+);
+
+/* アイキャッチ画像を使用可能にする */
+add_theme_support('post-thumbnails');
+
+/* 個別に記載された関数のインクルード */
 include_once('functions/cnt_include_webfonts.php');
 include_once('functions/cnt_get_header_img_cssstyle.php');
 include_once('functions/cnt_logowidget.php');
@@ -13,8 +25,20 @@ include_once('functions/cnt_get_page_title.php');
 include_once('functions/cnt_breadcrumb.php');
 include_once('functions/cnt_get_sns_link.php');
 include_once('functions/cnt_get_copyright_statement.php');
+include_once('functions/cnt_get_customposts.php');
+include_once('functions/cnt_get_posts.php');
+include_once('functions/cnt_pagenavi.php');
+include_once('functions/cnt_post_has_archive.php');
+include_once('functions/cnt_get_post_background.php');
+include_once('functions/cnt_get_post_thumbnail.php');
+include_once('functions/cnt_get_searchresult_background.php');
+include_once('functions/cnt_post_has_archive.php');
+include_once('functions/cnt_tocwidget.php');
 
 /* サイト全体の設定項目を管理する設定ページ */
 include_once('cnt-settings.php');
+
+/* 検索フォームにHTML5マークアップのサポートを追加 */
+add_theme_support('html5', array('search-form'));
 
 echo 'function.phpの読み込みが終了しました。';

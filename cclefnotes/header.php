c9bc78294b3a1559d215a2ca098df16474e516f3
cclefjp <cclefjp@outlook.com>
2021-10-19 22:28:48 +0900

    test

--- cclefnotes/header.php
+++ cclefnotes/header.php
@@ -6,6 +6,7 @@
         <meta name="viewport" content="width=device-width,initial-scale=1.0" />
         <?php echo cnt_include_webfonts(); ?>
         <link rel="stylesheet" href="<?php echo get_stylesheet_uri(); ?>">
+        <?php echo the_permalink(); ?>
         <title><?php get_the_title(); ?></title>
         <?php wp_head(); ?>
     </head>

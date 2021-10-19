03cb789a80b5dbde9eac5f8b1f932edba881fbd4
cclefjp <cclefjp@outlook.com>
2021-10-19 10:35:24 +0900

    includeを追加

--- cclefnotes/functions.php
+++ cclefnotes/functions.php
@@ -2,7 +2,16 @@
 /* functions.php - cclef notes theme用の関数群 */
 
 echo 'functions.phpを読み込んでいます。';
-// tfp_include_webfonts()
+
 include_once('functions/cnt_include_webfonts.php');
+include_once('functions/cnt_get_header_img_cssstyle.php');
+include_once('functions/cnt_logowidget.php');
+include_once('functions/cnt_get_title_class.php');
+include_once('functions/cnt_get_header_font_color.php');
+include_once('functions/cnt_get_title_font_family.php');
+include_once('functions/cnt_get_page_title.php');
+include_once('functions/cnt_breadcrumb.php');
+include_once('functions/cnt_get_sns_link.php');
+
 
 echo 'function.phpの読み込みが終了しました。';

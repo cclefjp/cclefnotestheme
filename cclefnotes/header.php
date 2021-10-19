3ae0c2d38545c1cb1b199dfabe6e351634e425b1
cclefjp <cclefjp@outlook.com>
2021-10-20 00:16:49 +0900

    デバッグメッセージを削除

--- cclefnotes/header.php
+++ cclefnotes/header.php
@@ -6,7 +6,6 @@
         <meta name="viewport" content="width=device-width,initial-scale=1.0" />
         <?php echo cnt_include_webfonts(); ?>
         <link rel="stylesheet" href="<?php echo get_stylesheet_uri(); ?>">
-        <?php echo the_permalink(); ?>
         <title><?php cnt_get_page_title(); ?></title>
         <?php wp_head(); ?>
     </head>

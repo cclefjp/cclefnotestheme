ba3b60392581e3c32e39db53eb35653cfb2b7486
cclefjp <cclefjp@outlook.com>
2021-10-21 11:28:38 +0900

    test

--- cclefnotes/functions/cnt_pagenavi.php
+++ cclefnotes/functions/cnt_pagenavi.php
@@ -13,7 +13,8 @@ function cnt_pagenavi( $the_query ) {
 	    'total' => $the_query->max_num_pages
     );
 
+    echo get_pagenum_link( $big );
     echo paginate_links( $param );
-    echo get_the_posts_pagination();
+    
 
 }

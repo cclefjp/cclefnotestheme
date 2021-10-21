699bc0db42a7fc8571cf2ec556bd567558f8ec9f
cclefjp <cclefjp@outlook.com>
2021-10-21 09:44:03 +0900

    debug

--- cclefnotes/functions/cnt_breadcrumb.php
+++ cclefnotes/functions/cnt_breadcrumb.php
@@ -83,6 +83,7 @@ function cnt_breadcrumb() {
         if (is_post_type_archive()) {
             //echo 'archive!';
         /* カスタム投稿タイプのアーカイブページの場合 */
+            $post_type = get_post_type();
             $archive_title = post_type_archive_title('', false);
             $post_type_obj = get_post_type_object($post_type);
             $archive_title = $post_type_obj->labels->menu_name;

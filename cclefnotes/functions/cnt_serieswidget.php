4d1ddd368e61a534cbbfc91ed0f301cdbf54d054
cclefjp <cclefjp@outlook.com>
2021-10-26 11:16:03 +0900

    無限ループを修正

--- cclefnotes/functions/cnt_serieswidget.php
+++ cclefnotes/functions/cnt_serieswidget.php
@@ -58,7 +58,7 @@ class CNT_SeriesWidget extends WP_Widget {
 
             while ( $the_query->have_posts() ) {
                 //$the_query->the_post();
-                echo '<li>' . '<a href="' . $the_query->the_permalink() . '">' . $the_query->the_title() . '</a></li>';
+                echo '<li>' . '<a href="' . $the_query->the_permalink() . '">' . $the_query->the_post() . '</a></li>';
             }
             
             echo '</ul>';
